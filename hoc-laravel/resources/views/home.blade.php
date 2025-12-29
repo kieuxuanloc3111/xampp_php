d9a2451887dfdc33fafeba906a49f6cd24701d76
kieuxuanloc3111 <kieuxuanloc3111@gmail.com>
2025-12-29 11:01:40 +0700

    laravel

---
+++ hoc-laravel/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>laravel</title>
+</head>
+<body>
+    <h1>chào</h1>
+    <h1>mình là lộc</h1>
+</body>
+</html>
