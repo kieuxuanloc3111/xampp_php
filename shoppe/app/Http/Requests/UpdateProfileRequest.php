23b8fe9602938cd450af2292e4359ccc6346b69d
kieuxuanloc3111 <kieuxuanloc3111@gmail.com>
2026-01-08 20:22:36 +0700

    a

--- shoppe/app/Http/Requests/UpdateProfileRequest.php
+++ shoppe/app/Http/Requests/UpdateProfileRequest.php
@@ -14,21 +14,13 @@ class UpdateProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // username: bắt buộc
             'name' => 'required|string|max:255',
 
-            // email: bắt buộc nhưng KHÔNG update
-            'email' => 'required|email',
-
-            // password: không bắt buộc
             'password' => 'nullable|min:6',
-
-            // các field khác không bắt buộc
             'phone' => 'nullable|string',
             'address' => 'nullable|string',
             'id_country' => 'nullable|integer',
 
-            // avatar: image < 1MB
             'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:1024',
         ];
     }
